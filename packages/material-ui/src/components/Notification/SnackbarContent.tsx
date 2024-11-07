6bce8d19db3e0f2befc32d343dd0e261f2f85b1d
william <>
2024-11-07 23:16:50 +0800

    fix: fix SnackbarContent bug

--- packages/material-ui/src/components/Notification/SnackbarContent.tsx
+++ packages/material-ui/src/components/Notification/SnackbarContent.tsx
@@ -1,4 +1,4 @@
-import { CustomContentProps, enqueueSnackbar } from 'notistack'
+import { CustomContentProps, useSnackbar } from 'notistack'
 import { Alert, Box } from '@mui/material'
 import IconButton from '@mui/material/IconButton'
 import CloseIcon from '@mui/icons-material/Close'
@@ -6,12 +6,13 @@ import React from 'react'
 
 export const SnackbarContent = React.forwardRef<HTMLDivElement, CustomContentProps>(
   ({ id, message, variant = 'default' }, ref) => {
+    const { closeSnackbar } = useSnackbar()
     return (
       <Box ref={ref} sx={{ maxWidth: '400px' }}>
         <Alert
           severity={variant === 'default' ? 'info' : variant}
           action={
-            <IconButton size="small" color="inherit" onClick={() => enqueueSnackbar.close(id)}>
+            <IconButton size="small" color="inherit" onClick={() => closeSnackbar(id)}>
               <CloseIcon fontSize="small" />
             </IconButton>
           }
